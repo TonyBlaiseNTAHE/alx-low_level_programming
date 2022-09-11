41272d611d0f31a7724d57ac0d00da4110d59f9b
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2022-09-11 08:45:20 -0700

    Task 0.Positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,10 +5,10 @@
 /**
  * main - Determines if a number is positive, negative or zero.
  *
- * Return: Always 0 (Success)
+ * Return:Always 0 (Success)
  */
 int main(void)
-{	
+{
 int n;
 
 srand(time(0));
