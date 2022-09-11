1ab784ff65f71a3a8187b4097597d9f6c405dfe8
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2022-09-11 10:28:47 -0700

    Correcting Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,13 @@
  */
 int main(void)
 {
-char alp[26]= "abcdefghijklmnopqrstuvwxyz";
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 int i;
-for (i = 0; ch < 26; i++)
+for (i = 0; i < 26; i++)
 {
 if (i != 5 && i != 17)
 {
-putchar(ch);
+putchar(alp[i]);
 }
 }
 putchar('\n');
