8951576ee480deba530fde004a5ec22b6a98d674
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2022-09-14 00:05:21 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - printing 10 times alphabet.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+int i;
+
+for (i = 0; i < 9; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+return (0);
+}
+}
