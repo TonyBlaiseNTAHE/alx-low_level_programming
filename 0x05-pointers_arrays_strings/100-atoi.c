04b92506dc3138c2f46e5cc48e4bb81188a4cd45
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2022-09-20 11:30:13 -0700

    Correcting task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
-/** 
- *_atoi - converts a string to an integer.
- * @s:input string.
- * Return:integer.
+/**
+ * _atoi - converts a string to an integer.
+ * @s: input string
+ * Return: integer
  */
 int _atoi(char *s)
 {
