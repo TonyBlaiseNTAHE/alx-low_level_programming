13320e46c85050622bbcd01d22703e0aa7b7e5e3
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2022-09-26 12:47:32 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,11 +9,13 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i = 0;
+	char *start = s;
 
-	for (i = 0; i < n; i++)
+	while (i < n)
 	{
-		s[i] = b;
+		**s++ = b;
+		i++;
 	}
-	return (s);
+	return (start);
 }
