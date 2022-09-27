012598390b7e7e302c651d5f943860c637381b2f
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2022-09-27 04:08:20 -0700

    Fixing 0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,14 +1,16 @@
 #ifndef _MAIN_H_
-#define _MAIN_H
+#define _MAIN_H_
 
+void set_string(char **s, char *to);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
-char *strpbrk(char *s, char *accept);
-int _putchar(char);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
-void printf_diagsums(int *a, int size);
-void set_string(char **s, char *to);
+void print_diagsums(int *a, int size);
+int _putchar(char);
+#include <stdio.h>
 
 #endif
