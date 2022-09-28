addaaf27981435ce29b93f730f0efeb0bc6bdb09
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2022-09-28 07:31:44 -0700

    Fixing 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 int find_sqrt(int num, int root);
-int _sprt_recursion(int n);
-
+int _sqrt_recursion(int n);
 
 /**
  * find_sqrt - Finds the natural square root of an inputted number.
- * @num: the number to find the square root of.
- * @root: the root to be tested.
+ * @num: The number to find the square root of.
+ * @root: The root to be tested.
  *
  * Return: If the number has a natural square root - the square root.
  *         If the number does not have a natural square root - -1.
@@ -22,3 +21,24 @@ int find_sqrt(int num, int root)
 
 	return (find_sqrt(num, root + 1));
 }
+
+/**
+ * _sqrt_recursion - Returns the natural square root of n.
+ * @n: the number to find the square root of.
+ *
+ * Return: If the number has a natural square root - the square root.
+ *         If the number does not have a natural square root - -1.
+ */
+int _sqrt_recursion(int n)
+{
+	int root = 0;
+
+	if (n < 0)
+		return (-1);
+
+	if (n == 1)
+		return (1);
+
+	return (find_sqrt(n, root));
+}
+
