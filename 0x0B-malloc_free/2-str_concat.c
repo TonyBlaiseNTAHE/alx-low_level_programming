4628046235c778b86aad492313645438dbcc67d1
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2022-10-04 08:17:59 -0700

    Fixing 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,7 +23,7 @@ char *str_concat(char *s1, char *s2)
 	for (j = 0; s2[j] != '\0'; j++)
 		;
 
-	strout + malloc(sizeof(char) * (i + j + 1));
+	strout = malloc(sizeof(char) * (i + j + 1));
 
 	if (strout == NULL)
 	{
@@ -32,7 +32,7 @@ char *str_concat(char *s1, char *s2)
 	}
 
 	for (k = 0; k < i; k++)
-		dtrout[k] = s1[k];
+		strout[k] = s1[k];
 
 	limit = j;
 	for (j = 0; j <= limit; k++, j++)
