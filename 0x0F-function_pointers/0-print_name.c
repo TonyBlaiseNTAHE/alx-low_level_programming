9836475134bd1403d98f5ce16ed5a23875a8ec1e
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2022-10-13 03:42:30 -0700

    First task

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,10 @@
 #include "function_pointers.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * print_name - function that prints my name
- * @name : name to be printed
- * @f: pointer to function
- * Return : nothing
+ * print_name - function that print a name
+ * @name: name to be printed
+ * @f: pointer to a function
+ * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
