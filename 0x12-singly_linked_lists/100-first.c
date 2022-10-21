d9ca5b6374e274c63f3b214c37a062921add9271
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2022-10-21 15:01:31 -0700

    100-first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * bmain - function excuted before main
+ * Return: no return
+ */
+void __attribute__ ((construtor)) bmain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back\n");
+}
