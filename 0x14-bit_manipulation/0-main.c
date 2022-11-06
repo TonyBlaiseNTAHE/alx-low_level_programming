15c45860da9483558067b495d0ce368155912eee
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2022-11-06 01:31:55 -0800

    0-binary_to_unit.c

---
+++ 0x14-bit_manipulation/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned int n;
+
+	n = binary_to_uint("1");
+	printf("%u\n", n);
+	n =  binary_to_uint("110010");
+	printf("%u\n", n);
+	return (0);
+}
