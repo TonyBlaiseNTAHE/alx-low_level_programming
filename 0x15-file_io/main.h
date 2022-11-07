a6c7bac561efdae8cf6dd4eb9a622001d05638dd
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2022-11-07 07:59:19 -0800

    0-red_textfile.c

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,4 @@
+VimCrypt~03!Ï˜NGÇ¤Ú©
+ÒñŠ”=¶\ˆý«Ož`…³“O1-ç ¨‚*ã,N¤ËdÝ[Gœê¢\^M?u€¥?Z ÂÜwXõ(ýPƒ%p–Yÿ
+ˆŸöíÃWdµ÷ÊXåµ—3ƒ¨CT$"Àv5æ†»Ú{ÖÏCNþy˜Ò´¥æ6ðÂgÒÞ‹	bóe0TcHõ[ùë"”"ÛÅ“¹7%àZ4’	ýEÛ«^’Öì[
+µrsD}®Ã”¯Î†2ÚÝ7Çûh®ÿ³ãÞ`ÑÚº:ÉWmÄöù|µ¶.µ*í—ÃŸ‘XO‚É>Í®«ÖGùñ¾,4V_ßÀscÊËu
